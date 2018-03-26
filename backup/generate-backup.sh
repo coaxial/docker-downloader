17c7d948fdd6687b18600c7c805a6f6e76a0b692
coaxial <hi@64b.it>
2018-03-26 09:07:17 -0400

    Reorder functions

--- backup/generate-backup.sh
+++ backup/generate-backup.sh
@@ -4,31 +4,6 @@
 # backed up
 # api docs: https://github.com/Sonarr/Sonarr/wiki/Command#backup
 
-err_exit() {
-  echo 'This script only take one argument: "sonarr", "radarr", or "hydra".'
-  exit 1
-}
-
-if [ "$#" -ne 1 ]; then
-  err_exit
-fi
-
-if [ "$1" = 'sonarr' ]; then
-  dir_name='sonarr'
-  url_path='series'
-  trigger_ar_backup
-elif [ "$1" = 'radarr' ]; then
-  dir_name='radarr'
-  url_path='movies'
-  trigger_ar_backup
-elif [ "$1" = 'hydra' ]; then
-  dir_name='hydra'
-  url_path='aggregator/nzb'
-  trigger_hydra_backup
-else
-  err_exit
-fi
-
 scheme="http"
 hostname="proxy"
 
@@ -55,3 +30,28 @@ trigger_hydra_backup() {
     --remote-header-name \
     "${scheme}://${hostname}/${url_path}/internalapi/backup"
 }
+
+err_exit() {
+  echo 'This script only take one argument: "sonarr", "radarr", or "hydra".'
+  exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+  err_exit
+fi
+
+if [ "$1" = 'sonarr' ]; then
+  dir_name='sonarr'
+  url_path='series'
+  trigger_ar_backup
+elif [ "$1" = 'radarr' ]; then
+  dir_name='radarr'
+  url_path='movies'
+  trigger_ar_backup
+elif [ "$1" = 'hydra' ]; then
+  dir_name='hydra'
+  url_path='aggregator/nzb'
+  trigger_hydra_backup
+else
+  err_exit
+fi
