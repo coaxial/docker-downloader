45c6422e7ba347ab0829c4b7870eec23ccf43ce7
coaxial <hi@64b.it>
2018-04-18 16:56:22 -0400

    Remove comment, use exec

--- subtitles/root/scripts/find-subs.sh
+++ subtitles/root/scripts/find-subs.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/with-contenv sh
-# su -s /bin/sh -c "\
-#   /usr/local/bin/subliminal \
-#   --addic7ed '${ADDIC7ED_USERNAME}' '${ADDIC7ED_PASSWORD}' \
-#   --opensubtitles '${OPENSUBS_USERNAME}' '${OPENSUBS_PASSWORD}' \
-#   download \
-#   --age 2h \
-#   --language en \
-#   --provider addic7ed \
-#   --provider opensubtitles \
-#   --verbose \
-#   /movies \
-#   /series \
-# 2>&1" "$(id -nu user)"
-s6-envuidgid "$(id -nu user)" \
+exec s6-envuidgid "$(id -nu user)" \
   "/usr/local/bin/subliminal \
   --addic7ed '${ADDIC7ED_USERNAME}' '${ADDIC7ED_PASSWORD}' \
   --opensubtitles '${OPENSUBS_USERNAME}' '${OPENSUBS_PASSWORD}' \
