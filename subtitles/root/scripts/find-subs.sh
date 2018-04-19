72f72039dd6df2f66495406f5d90898c8b53da00
Pierre <coaxial@users.noreply.github.com>
2018-04-18 22:07:34 -0400

    Use jobber to run subliminal (#25)
    * Use jobber to run subliminal
    
    * Use example env vars file
    
    so docker-compose can run on CI without committing a dummy .env file
    
    * Use more recent Alpine to get the shadow package
    
    * Enforce custom UID GID
    
    * Set log dirs permissions
    
    The s6 logger runs as nobody
    
    * Remove comment, use exec
    
    * Use non blocking mode
    
    * Specify cache
    
    * Remove exec
    
    * Remove quotes
    
    * Use exec, remove redundant chown

---
+++ subtitles/root/scripts/find-subs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv sh
+exec s6-envuidgid "$(id -nu user)" \
+  /usr/local/bin/subliminal \
+  --addic7ed "${ADDIC7ED_USERNAME}" "${ADDIC7ED_PASSWORD}" \
+  --opensubtitles "${OPENSUBS_USERNAME}" "${OPENSUBS_PASSWORD}" \
+  --cache-dir /var/cache/subliminal \
+  download \
+  --age 2h \
+  --language en \
+  --provider addic7ed \
+  --provider opensubtitles \
+  --verbose \
+  /movies \
+  /series \
+ 2>&1 | s6-log -v /var/log/subliminal
